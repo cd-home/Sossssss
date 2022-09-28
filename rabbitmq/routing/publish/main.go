c475a66ce865e6a2b7edc02b3f06f44570bac356
GodYao <liyaoo1995@gmail.com>
2022-09-28 15:30:02 +0800

    😀🌴⭐🌎☀️🌈🌽: RabbitMQ routing

---
+++ rabbitmq/routing/publish/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://rabbitmq:rabbitmq@10.211.55.18:5672")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = ch.ExchangeDeclare("logs_direct", "direct", true, false, false, false, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = ch.Publish("logs_direct", "info", false, false, amqp.Publishing{
+		ContentType: "",
+		Body:        []byte("Hello"),
+	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
---
+++ rabbitmq/routing/subscribe/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main() {
+	name := flag.String("c", "c", "consumer name")
+	queue := flag.String("q", "q", "queue name")
+	key := flag.String("k", "k", "routing key")
+	flag.Parse()
+	conn, err := amqp.Dial("amqp://rabbitmq:rabbitmq@10.211.55.18:5672")
+	if err != nil {
+		log.Println(err)
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Println(err)
+	}
+	err = ch.ExchangeDeclare("logs_direct", "direct", true, false, false, false, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	q, err := ch.QueueDeclare(*queue, false, false, true, false, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = ch.QueueBind(q.Name, *key, "logs_direct", false, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	msgs, err := ch.Consume(q.Name, *name, true, false, false, false, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for msg := range msgs {
+		log.Printf(" [x] %s", msg.Body)
+	}
+}
