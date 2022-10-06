6b7a916a8084aea875ff3dca0e05650222da121e
GodYao <liyaoo1995@gmail.com>
2022-10-06 22:58:24 +0800

    😀🌴⭐🌎☀️🌈🌽: Add Elastic Project

---
+++ elastic/go.mod
@@ -0,0 +1,8 @@
+module elastic
+
+go 1.19
+
+require (
+	github.com/elastic/elastic-transport-go/v8 v8.1.0 // indirect
+	github.com/elastic/go-elasticsearch/v8 v8.4.0 // indirect
+)
--- elastic/main.go
+++ elastic/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"crypto/tls"
+	"github.com/elastic/elastic-transport-go/v8/elastictransport"
+	"github.com/elastic/go-elasticsearch/v8"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
 
+func main() {
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+		},
+		Username: "foo",
+		Password: "bar",
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost:   10,
+			ResponseHeaderTimeout: time.Second,
+			DialContext:           (&net.Dialer{Timeout: time.Second}).DialContext,
+			TLSClientConfig: &tls.Config{
+				MinVersion: tls.VersionTLS12,
+			},
+		},
+	}
+
+	es, _ := elasticsearch.NewClient(cfg)
+	log.Print(es.Transport.(*elastictransport.Client).URLs())
 }
