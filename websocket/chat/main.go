36b8bafc4f67745d4b6c433f9537a3176a20c235
GodYao <liyaoo1995@gmail.com>
2022-10-12 15:40:25 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Websocket chat

--- websocket/chat/main.go
+++ websocket/chat/main.go
@@ -2,45 +2,110 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"net/http"
 	"nhooyr.io/websocket"
-	"nhooyr.io/websocket/wsjson"
-	"strconv"
+	"sync"
 	"time"
 )
 
+var ChatManager *Chat
+
+type Chat struct {
+	mu         sync.Mutex
+	subscriber map[*subscriber]struct{}
+}
+
+type subscriber struct {
+	ws    *websocket.Conn
+	msg   chan []byte
+	close func()
+	stop  <-chan struct{}
+}
+
+func init() {
+	ChatManager = &Chat{
+		subscriber: make(map[*subscriber]struct{}),
+	}
+}
+
 func main() {
-	http.HandleFunc("/ws", func(w http.ResponseWriter, req *http.Request) {
-		conn, err := websocket.Accept(w, req, &websocket.AcceptOptions{
-			InsecureSkipVerify: true,
-		})
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		ctx := context.Background()
+	// static file service
+	fs := http.FileServer(http.Dir("./static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/sub", Subscribe)
+	http.HandleFunc("/pub", Publisher)
+
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func Subscribe(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Accept(w, r, &websocket.AcceptOptions{InsecureSkipVerify: true})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	suber := &subscriber{
+		ws:  ws,
+		msg: make(chan []byte),
+		close: func() {
+			//ws.Close(websocket.StatusPolicyViolation, "conn slow")
+		},
+	}
+	ChatManager.mu.Lock()
+	ChatManager.subscriber[suber] = struct{}{}
+	ChatManager.mu.Unlock()
+	go subscribe(context.Background(), suber)
+}
+
+func subscribe(ctx context.Context, suber *subscriber) {
+	heartBeat := func() (<-chan time.Time, <-chan struct{}) {
+		ch := make(chan time.Time, 1)
+		tk := time.NewTicker(2 * time.Second)
+		stop := make(chan struct{})
 		go func() {
+			defer tk.Stop()
+			defer close(stop)
 			for {
-				var v interface{}
-				err = wsjson.Read(ctx, conn, &v)
-				if err != nil {
-					log.Println(err)
+				select {
+				case <-stop:
 					return
+				case t := <-tk.C:
+					ch <- t
 				}
-				log.Printf("From Client：%v\n", v)
 			}
 		}()
-		for i := 0; i < 10; i++ {
-			err = wsjson.Write(ctx, conn, "To WebSocket Client "+strconv.Itoa(i))
-			time.Sleep(time.Second)
-			if err != nil {
-				log.Println(err)
-				return
-			}
+		return ch, stop
+	}
+	beat, stop := heartBeat()
+	suber.stop = stop
+	for {
+		select {
+		case msg := <-suber.msg:
+			_ = suber.ws.Write(ctx, websocket.MessageText, msg)
+		case t := <-beat:
+			_ = suber.ws.Write(ctx, websocket.MessageText, []byte(t.Format("20060102150405")))
 		}
-		//conn.Close(websocket.StatusNormalClosure, "")
-	})
+	}
+}
 
-	log.Fatal(http.ListenAndServe(":9999", nil))
+func Publisher(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 200)
+		return
+	}
+	// 广播
+	for suber := range ChatManager.subscriber {
+		select {
+		case suber.msg <- body:
+		}
+	}
+	w.WriteHeader(http.StatusAccepted)
 }
