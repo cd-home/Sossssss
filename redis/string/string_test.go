6745d9b1086f4d427e4698b371a7930cc656163d
GodYao <liyaoo1995@gmail.com>
2022-10-21 10:54:53 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Redis [string]

--- redis/string/string_test.go
+++ redis/string/string_test.go
@@ -1,7 +1,45 @@
 package string
 
-import "testing"
+import (
+	"context"
+	"github.com/go-redis/redis/v9"
+	"redis/client"
+	"testing"
+	"time"
+)
 
-func TestGet(t *testing.T) {
-	t.Log(Get("stock"))
+var cli *redis.Client
+var ctx context.Context
+
+func init() {
+	cli = client.NewSimpleClient()
+	ctx = context.Background()
+}
+
+func TestStringSet(t *testing.T) {
+	// cli.SetEx()
+	err := cli.Set(ctx, "stock", 10, time.Second*10).Err()
+	if err != nil {
+		t.Log(err)
+	}
+}
+
+func TestStringGet(t *testing.T) {
+	value, err := cli.Get(ctx, "stock").Result()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(value)
+}
+
+func TestStringSetNX(t *testing.T) {
+	for i := 10; i > 0; i-- {
+		// 不存在即设置, 否则不做任何操作
+		err := cli.SetNX(ctx, "stock", i, time.Second*10).Err()
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	// 存在时才能设置
+	//cli.SetXX()
 }
