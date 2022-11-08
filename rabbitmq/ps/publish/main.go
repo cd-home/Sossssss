bd3f8605cf704c3b727787856626631649fa6817
GodYao <liyaoo1995@gmail.com>
2022-11-08 20:41:26 +0800

    😀🌴⭐🌎☀️🌈🌽: Update RabbitMQ [Ps, NotifyReturn]

--- rabbitmq/ps/publish/main.go
+++ rabbitmq/ps/publish/main.go
@@ -25,14 +25,37 @@ func main() {
 		log.Println(err)
 		return
 	}
+	// 首先投递失败存在如下两个情况
+	// 1. 交换器无法根据自身的类型和路由键匹配到队列
+	// 2. 交换器将消息路由到队列时, 发现队列上并不存在任何消费者
+
+	// 监听投递失败的场景
+	// 生产者投递消息时指定 mandatory 或者 immediate 参数设为 true
+	// RabbitMQ 会把无法投递的消息通过 Basic.Return 命令将消息返回给生产者
+	notifyReturn := ch.NotifyReturn(make(chan amqp.Return, 1))
+	go func() {
+		for {
+			body, ok := <-notifyReturn
+			if ok {
+				fmt.Println("no push", string(body.Body))
+			}
+		}
+	}()
+
 	for i := 0; i < 1000; i++ {
 		t := time.Now().Format(time.RFC3339)
-		err = ch.PublishWithContext(context.Background(), "logs-fanout", "", false, false, amqp.Publishing{
-			ContentType: "",
-			Body:        []byte(t),
-			// 消息持久化
-			DeliveryMode: amqp.Persistent,
-		})
+		err = ch.PublishWithContext(
+			context.Background(),
+			"logs-fanout", "",
+			true,
+			false,
+			amqp.Publishing{
+				ContentType: "",
+				Body:        []byte(t),
+				// 消息持久化
+				DeliveryMode: amqp.Persistent,
+				// 可以设置消息的过期时间
+			})
 		if err != nil {
 			log.Println(err)
 			return
